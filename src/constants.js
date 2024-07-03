741933465c957b2fcf8b2b292194e83531f7dfab
Aksoni01 <soni.akshat01@gmail.com>
2024-07-03 19:50:42 +0530

    connect to MongoAtlas

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+
+export const DB_NAME = "utube_db"  // aplication db name
---
+++ src/index.js
@@ -0,0 +1,42 @@
+//require('dotenv').config({path : './env'}) 
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : './env'
+})
+
+connectDB()
+
+
+
+
+
+
+/* first apporoach 
+
+
+import express, { application } from "express"
+const app= express()
+
+// use try catch and asyac await while interact with DB (because db may be held at other continent)
+
+// IFFE
+;( async () =>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+       app.on("error",(error) => {
+        console.log("ERRR:",error);
+        throw error
+       })
+
+       app.listen(process.env.PORT,() =>{
+        console.log(`App is listen on ${process.env.PORT}`);
+       })
+    } catch (error) {
+        console.log("Error",error);
+    }
+})()
+
+*/
